e6791abbe169a2fd3f767b8205e609bcc33c9b28
GKastet <GKastet@users.noreply.github.com>
2023-10-13 14:27:36 +0000

    deploy: fbd6ee1b9b6dc11dbe3649063f1cf5386442fdde

---
+++ assets/index-0823abd6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e2bac23c.js";const o=()=>e.jsx(e.Fragment,{children:e.jsx("h2",{children:"HomePage"})}),t=o;export{t as default};
--- assets/index-0d1f0411.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-5dc9a190.js";const o=()=>e.jsx(e.Fragment,{children:e.jsx("h2",{children:"HomePage"})}),t=o;export{t as default};
